e6996a3546916f21f2353e5b8f79515f4c120c29
Mahrud Sayrafi <mahrud@berkeley.edu>
2021-06-29 12:33:33 -0500

    backporting changes from Macaulay2/M2#2160

--- src/mathicgb/ScopeExit.hpp
+++ src/mathicgb/ScopeExit.hpp
@@ -17,13 +17,13 @@ public:
 private:
   friend struct GuardMaker;
   Guard(T&& action, const bool& active):
-    mAction(std::move(action)), mOwning(true), mActive(active) {}
+    mOwning(true), mActive(active), mAction(std::move(action)) {}
 
   // Most compilers should elide the call to this construtor, but it must be
   // here anyway and we should support even a crazy compiler that decides to
   // call it.
   Guard(Guard<T>&& guard):
-    mAction(std::move(guard.mAction)), mOwning(true), mActive(guard.mActive)
+    mOwning(true), mActive(guard.mActive), mAction(std::move(guard.mAction))
   {
     assert(guard.mActive);
     guard.mOwning = false; // to avoid calling mAction twice
@@ -95,6 +95,7 @@ MATHICGB_NAMESPACE_END
 // when using MYLIB_SCOPE_EXIT without a name parameter. Not very happy about
 // turning off the warning. I wonder if there is a way to avoid the warning in
 // this case without turning it off.
-#pragma warning (disable: 4003)
+// MES: turning this off for now, as it gives warning on clang (unknown pragma ignored).
+// #pragma warning (disable: 4003)
 
 #endif
